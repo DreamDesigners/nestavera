56f1cb729ca0ffaa5aecff02d17e8b69e2d78a1d
HarshSonawane <sonawaneharshwardhan@gmail.com>
2022-12-12 23:37:23 +0530

    admin updated

--- career/admin.py
+++ career/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Job, JobLocation, JobCategory
+from .models import Job, JobLocation, JobCategory, JobApplication
 
 admin.site.register(JobLocation)
 admin.site.register(JobCategory)
 admin.site.register(Job)
+admin.site.register(JobApplication)
