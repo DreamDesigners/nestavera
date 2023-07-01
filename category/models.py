cea5f3e93475cf439fbb569d15519a7cdf68277e
HarshSonawane <sonawaneharshwardhan@gmail.com>
2023-07-01 23:37:20 +0530

    added the migrations for file

--- category/models.py
+++ category/models.py
@@ -21,6 +21,7 @@ class Category(models.Model):
   parent        = models.ForeignKey("self", on_delete=models.SET_NULL, null=True, blank=True)
   title         = models.CharField(max_length=255, unique=True)
   slug          = models.SlugField(max_length=500, unique=True)
+  file          = models.FileField(upload_to='category/', blank=True, null=True)
 
   is_active     = models.BooleanField(default=True)
   created_at    = models.DateTimeField(auto_now_add=True)
