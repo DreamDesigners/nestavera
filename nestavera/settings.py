e5c79e95cdc167fc180ef5c5943a04a3555684a9
HarshSonawane <sonawaneharshwardhan@gmail.com>
2023-07-17 01:49:50 +0530

    removed secrets from code

--- nestavera/settings.py
+++ nestavera/settings.py
@@ -177,7 +177,7 @@ REST_FRAMEWORK = {
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "notifyddsio@gmail.com"
-EMAIL_HOST_PASSWORD = "dyhzpnbspzymwwue"
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
